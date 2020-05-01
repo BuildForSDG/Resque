4b01067e5b50e2e369b0ea16750647f53bf526db
bytenaija <everistusolumese@gmail.com>
2020-05-01 11:09:42 +0100

    Setup an express server

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,31 @@
-const app = async () => '#BuildforSDG';
+const express = require('express');
+const cors = require('cors');
 
-export default app;
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('alarm', (msg) => {
+    console.log('blast alarm: ' + msg);
+  });
+});
+
+const PORT = process.env.PORT || 5006;
+
+http.listen(PORT, () => {
+  console.log(`App connected on port ${PORT}`);
+});
